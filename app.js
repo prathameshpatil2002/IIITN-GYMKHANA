130365b56186cb299cac3e9b7874cfd272a7c4f9
prathamesh patil <prathm.patil.100@gmail.com>
2022-07-30 23:31:21 +0530

    IIITN Gymkhana Project complete

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const PORT = 3000;
+
+const app = express();
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+app.use(bodyParser.json());
+
+const index = require("./routes/index");
+const contact = require("./routes/contact");
+const fests = require("./routes/fests");
+const facilities = require("./routes/facilities");
+const office = require("./routes/office");
+const sports = require("./routes/sports");
+const cultural = require("./routes/cultural");
+const loginpage = require("./routes/loginpage");
+const ace = require("./routes/ace");
+const probe = require("./routes/probe");
+const orator = require("./routes/orator");
+
+app.use('/', index)
+app.use('/index',index)
+app.use('/contact', contact)
+app.use('/sports', sports)
+app.use('/cultural', cultural)
+app.use('/office', office)
+app.use('/fests', fests)
+app.use('/facilities', facilities)
+app.use('/login',loginpage)
+app.use('/ace',ace)
+app.use('/probe',probe)
+app.use('/orator',orator)
+
+
+app.use(express.static(__dirname + '/views'));
+
+app.listen(PORT, () => {
+    console.log("server is listening to http://localhost:3000");
+}) 
